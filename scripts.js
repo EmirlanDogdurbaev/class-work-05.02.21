63a09a3cedca50dba9c8d6e918fee0e864164692
EmirlanDogdurbaev <jigit38@gmail.com>
2021-02-05 15:38:20 +0300

    created style

--- scripts.js
+++ scripts.js
@@ -21,52 +21,70 @@
 // lii.append(newlii);
 // butt.append(newbutt);
 
-// console.log()
+// // console.log()
 
-let ex1 = document.querySelector(".ex1")
-ex1.style.border = "solid black 1px";
-ex1.style.width = "50px";
-ex1.style.padding = "5px 10px";
-ex1.style.textAlign = "center"
-ex1.style.marginBottom = "15px"
-
-
-let ex2 = document.querySelector(".ex2")
-ex2.style.border = " dotted black 5px";
-ex2.style.backgroundColor = "Red"
-ex2.style.width = "50px";
-ex2.style.padding = "5px 10px";
-ex2.style.textAlign = "center"
-ex2.style.margingTop = "15px"
-ex2.style.marginBottom = "15px"
-
-let ex3 = document.querySelector(".ex3")
-ex3.style.border = " dotted aqua 5px";
-ex3.style.color = "blue"
-ex3.style.width = "50px";
-ex3.style.padding = "5px 10px";
-ex3.style.textAlign = "center"
-ex3.style.margingTop = "15px"
-ex3.style.marginBottom = "15px"
-
-let ex4 = document.querySelector(".ex4")
-ex4.style.border = "none";
-ex4.style.color = "black"
-ex4.style.backgroundColor = "tomato"
-ex4.style.width = "50px";
-ex4.style.padding = "15px 20px";
-ex4.style.textAlign = "center"
-ex4.style.margingTop = "15px"
-ex4.style.marginBottom = "15px"
+// let ex1 = document.querySelector(".ex1")
+// ex1.style.border = "solid black 1px";
+// ex1.style.width = "50px";
+// ex1.style.padding = "5px 10px";
+// ex1.style.textAlign = "center"
+// ex1.style.marginBottom = "15px"
 
 
+// let ex2 = document.querySelector(".ex2")
+// ex2.style.border = " dotted black 5px";
+// ex2.style.backgroundColor = "Red"
+// ex2.style.width = "50px";
+// ex2.style.padding = "5px 10px";
+// ex2.style.textAlign = "center"
+// ex2.style.margingTop = "15px"
+// ex2.style.marginBottom = "15px"
 
-let ex5 = document.querySelector(".ex5")
-ex5.style.border = " solid black 5px";
-ex5.style.backgroundColor = "black"
-ex5.style.width = "100px";
-ex5.style.color = "white"
-ex5.style.padding = "5px 10px";
-ex5.style.textAlign = "center"
-ex5.style.margingTop = "15px"
-ex5.style.marginBottom = "15px"
+// let ex3 = document.querySelector(".ex3")
+// ex3.style.border = " dotted aqua 5px";
+// ex3.style.color = "blue"
+// ex3.style.width = "50px";
+// ex3.style.padding = "5px 10px";
+// ex3.style.textAlign = "center"
+// ex3.style.margingTop = "15px"
+// ex3.style.marginBottom = "15px"
+
+// let ex4 = document.querySelector(".ex4")
+// ex4.style.border = "none";
+// ex4.style.color = "black"
+// ex4.style.backgroundColor = "tomato"
+// ex4.style.width = "50px";
+// ex4.style.padding = "15px 20px";
+// ex4.style.textAlign = "center"
+// ex4.style.margingTop = "15px"
+// ex4.style.marginBottom = "15px"
+
+
+
+// let ex5 = document.querySelector(".ex5")
+// ex5.style.border = " solid black 5px";
+// ex5.style.backgroundColor = "black"
+// ex5.style.width = "100px";
+// ex5.style.color = "white"
+// ex5.style.padding = "5px 10px";
+// ex5.style.textAlign = "center"
+// ex5.style.margingTop = "15px"
+// ex5.style.marginBottom = "15px"
+
+
+// example1.onlick = example;
+// example1.addEventListener('click', example);
+// example1.addEventListener('click', );
+
+// function example(){
+//     alert("Clicked")
+// }
+
+
+
+let btn = document.querySelector("button")
+btn.style.border = "none";
+btn.addEventListener('click', function(){
+   this.style.backgroundColor = "red"
+}
+ );
